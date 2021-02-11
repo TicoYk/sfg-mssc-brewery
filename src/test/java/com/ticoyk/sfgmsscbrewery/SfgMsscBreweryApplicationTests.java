298b2d432b452a271055ccc4049a1e6b141fc400
Dídimo <didimoykym@gmail.com>
2021-02-11 15:10:42 -0300

    Initial commit

---
+++ src/test/java/com/ticoyk/sfgmsscbrewery/SfgMsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticoyk.sfgmsscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgMsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
